e1b2cbb192c7abbe577d93d900dbad00ec63399d
es3990 <es3990@nyu.edu>
2018-10-14 15:56:30 -0400

    Basic UI

--- iOS/passtheaux/passtheaux/ViewController.swift
+++ iOS/passtheaux/passtheaux/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Handle the text field's user input through delegate callbacks
+        searchTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        outputLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func search(_ sender: UIButton) {
+        outputLabel.text = "Default Text"
+    }
 }
 
